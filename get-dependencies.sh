d5d975a04e9e0f8f2d512e021928d409c422124b
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-09-04 18:24:43 -0400

    test
    Comment out debloated package installation steps and remove mesa package.

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -61,8 +61,10 @@ echo "---------------------------------------------------------------"
 	CARGO_INCREMENTAL=0 cargo test --locked --offline
 )
 
-echo "Installing debloated packages..."
-echo "---------------------------------------------------------------"
-wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
-chmod +x ./get-debloated-pkgs.sh
-./get-debloated-pkgs.sh --add-opengl --prefer-nano
+#echo "Installing debloated packages..."
+#echo "---------------------------------------------------------------"
+#wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
+#chmod +x ./get-debloated-pkgs.sh
+#./get-debloated-pkgs.sh --add-opengl --prefer-nano
+
+pacman -Rsndd --noconfirm mesa
